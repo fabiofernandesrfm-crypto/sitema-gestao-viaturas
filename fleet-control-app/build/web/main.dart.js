4acd8bed7ccdeeeec31a00a95e138c0b3034427f
fabiofernandesrfm-crypto <fabiofernandesrfm@gmail.com>
2026-07-27 19:56:03 -0300

    Add build/web folder and update Dockerfile for deployment

--- fleet-control-app/build/web/main.dart.js
+++ fleet-control-app/build/web/main.dart.js
@@ -32182,7 +32182,7 @@ $S:2}
 B.aGg.prototype={
 $1(a){var s=B.e_().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/69c8c61792f04cc809dfef0c910414fb9afc06cd/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/0cd610717bde95fd88343c64f81c11ba4e5c0010/":s)+a},
 $S:124}
 B.Ne.prototype={
 a_T(){var s=this.aiP(),r=$.bC.bx().ImageFilter.MakeColorFilter(s,null)
